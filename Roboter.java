d52ace120edf30940490af614e8c3243fa69b876
richard <richard.hempel99@gmail.com>
2020-11-05 22:06:26 +0100

    Klasse Roboter

---
+++ Roboter.java
@@ -0,0 +1,34 @@
+
+
+public class Roboter
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Roboter
+     */
+    public Roboter()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public void spracherkennung()
+    {
+        while( spracherkennung.size() < aH )
+        System.out.ptintln ("Stellen Sie eine Frage");
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+      
+        return x + y;
+    }
+}
